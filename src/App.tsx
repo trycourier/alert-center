ea1b07704bb65b892dbaa0baf4cd7d0f025b930e
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-01-24 18:29:03 +0400

    Set up Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => loginWithRedirect()}>Log In</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
