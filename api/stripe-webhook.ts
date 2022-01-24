105c4fada2de9b2be549df69ec3fb29822770ae3
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-01-24 19:40:17 +0400

    Init Stripe Webhook

--- api/stripe-webhook.ts
+++ api/stripe-webhook.ts
@@ -1,8 +1,50 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import Stripe from "stripe";
+import getRawBody from "raw-body";
 
-const stripeWebhook = (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: "2020-08-27",
+});
+
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+
+const stripeWebhook = async (
+  request: VercelRequest,
+  response: VercelResponse
+) => {
+  if (request.method !== "POST") {
+    return response.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    const signature = request.headers["stripe-signature"];
+
+    const body = await getRawBody(request);
+
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      endpointSecret
+    );
+
+    if (event.type !== "charge.succeeded") {
+      throw new Error("Unhandled event type");
+    }
+
+    const charge = event.data.object;
+
+    console.log(charge);
+
+    response.status(200).json({ success: true });
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 };
 
 export default stripeWebhook;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
