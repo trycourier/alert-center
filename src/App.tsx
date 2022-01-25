5ab509468754220c9e8d8feb1970fa05aa082c65
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-01-25 19:43:00 +0400

    Test env variables on Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container, Box, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Input,
+  Tag,
+  AlertTitle,
+  AlertDescription,
+  Alert,
+  AlertIcon,
+  Code,
+  Link,
+  CloseButton,
+} from "@chakra-ui/react";
 import { ReactComponent as Logo } from "./logo.svg";
 
 function App() {
@@ -7,23 +23,77 @@ function App() {
     useAuth0();
 
   return (
-    <Container py={10} textAlign="center">
-      <Box maxW={32} mx="auto">
-        <Logo />
-      </Box>
-
-      <Heading my={8}>Alert Center</Heading>
-
-      <Button
-        isLoading={isLoading}
-        onClick={isAuthenticated ? () => logout() : () => loginWithRedirect()}
-        colorScheme="purple"
-        variant={isAuthenticated ? "outline" : "solid"}
+    <Container py={{ base: 6, md: 10 }}>
+      <Flex
+        mb={8}
+        direction={{ base: "column", md: "row" }}
+        alignItems="center"
       >
-        {isAuthenticated ? "Log out" : "Log In"}
-      </Button>
+        <Flex alignItems="flex-end">
+          <Box w={32}>
+            <Logo />
+          </Box>
+          <Tag ml={4} colorScheme="purple">
+            <Heading as="h1" size="sm">
+              Alert Center
+            </Heading>
+          </Tag>
+        </Flex>
 
-      {isAuthenticated && <Text mt={4}>Hi, {user?.name}</Text>}
+        <Flex ml={{ md: "auto" }} pt={{ base: 6, md: 0 }} alignItems="center">
+          <Text mr={2}>{user && `Hi, ${user.given_name}!`}</Text>
+
+          <Button
+            size="sm"
+            isLoading={isLoading}
+            onClick={
+              isAuthenticated ? () => logout() : () => loginWithRedirect()
+            }
+            colorScheme="purple"
+            variant={isAuthenticated ? "outline" : "solid"}
+          >
+            {isAuthenticated ? "Log out" : "Log In"}
+          </Button>
+        </Flex>
+      </Flex>
+
+      {isAuthenticated && (
+        <>
+          <Alert
+            my={8}
+            status="info"
+            variant="left-accent"
+            alignItems="flex-start"
+            colorScheme="gray"
+          >
+            <AlertIcon />
+            <Box>
+              <AlertTitle mb={1}>Stripe Webhook URL</AlertTitle>
+              <AlertDescription>
+                Create a{" "}
+                <Link
+                  href="https://dashboard.stripe.com/test/webhooks/create"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Stripe Webhook
+                </Link>{" "}
+                with following Endpoint URL and select{" "}
+                <Code colorScheme="blackAlpha">charge.succeeded</Code> event to
+                listen to.
+              </AlertDescription>
+              <Input
+                mt={2}
+                value={`${process.env.REACT_APP_VERCEL_URL}/api/stripe-webhook`}
+                variant="outline"
+                readOnly
+                bgColor="white"
+              />
+              <CloseButton position="absolute" right={2} top={2} />
+            </Box>
+          </Alert>
+        </>
+      )}
     </Container>
   );
 }
